3e6eccbdd6dd2a1960dc7fe2e06ccb63e5ad7d41
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2020-12-13 22:04:48 +0100

    :rotating_light: code plus beau

--- src/main/java/lpdioc/tictactoe/GameFactory.java
+++ src/main/java/lpdioc/tictactoe/GameFactory.java
@@ -3,22 +3,21 @@ package lpdioc.tictactoe;
 
 public class GameFactory {
 
-	public static Game createGame(String name, int nbCols, int nbRows) {
-		switch (name.toLowerCase()) {
-		case "tictactoe":
-			return new TicTacToe(nbCols, nbRows);
-		default:
-			throw new IllegalArgumentException(name + " is not a supported game");
-		}
-	}
+    private GameFactory() {
+        // pour empêcher la création d'une instance de cette classe
+    }
 
-	public static Game createGame(String name, String board) {
-		switch (name.toLowerCase()) {
-		case "tictactoe":
-			return new TicTacToe(board);
-		default:
-			throw new IllegalArgumentException(name + " is not a supported game");
-		}
-	}
+    public static Game createGame(String name, int nbCols, int nbRows) {
+        if ("tictactoe".equals(name.toLowerCase())) {
+            return new TicTacToe(nbCols, nbRows);
+        }
+        throw new IllegalArgumentException(name + " is not a supported game");
+    }
 
+    public static Game createGame(String name, String board) {
+        if ("tictactoe".equals(name.toLowerCase())) {
+            return new TicTacToe(board);
+        }
+        throw new IllegalArgumentException(name + " is not a supported game");
+    }
 }
